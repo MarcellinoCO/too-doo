7887a42108756c136dd04fcc91676f53be453664
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2023-10-26 23:45:44 +0800

    feat: setup bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import Filters from "./components/Filter";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="container mt-5">
+      <h1 className="mb-4 fw-bold">Let&apos;s get things done!</h1>
+      <div className="input-group mb-4">
+        <input className="form-control" type="text" placeholder="What to do" />
+        <button
+          className="btn btn-primary px-3 text-white fw-bold"
+          type="button"
+        >
+          <i className="bi bi-plus-circle me-1" /> Add
+        </button>
+      </div>
+
+      <hr />
+
+      <div className="d-flex justify-content-end align-items-baseline mb-2">
+        <p>Filter:</p>
+        <Filters />
+      </div>
+
+      <ul className="list-group">
+        <li className="list-group-item d-flex justify-content-between align-items-center">
+          <span>Buy milk</span>
+          <div className="btn-group" role="group">
+            <button className="btn btn-outline-success btn-sm" type="button">
+              Mark as done <i className="bi bi-check-circle ms-1" />
+            </button>
+            <button className="btn btn-danger btn-sm" type="button">
+              <i className="bi bi-trash text-white" />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./index.css";
 import App from "./App";
 
 import store from "./store/store";
 import { Provider } from "react-redux";
+
+import "./styles.scss";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
